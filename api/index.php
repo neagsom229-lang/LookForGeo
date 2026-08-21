e8ab7114ac8f38e43c5ce7e1b002c2423a4b07e3
Chheangsamnang <chheangsamnang.wu@gmail.com>
2026-08-21 21:11:37 +0700

    Switch to Render PHP environment

--- api/index.php
+++ api/index.php
@@ -1,18 +1,14 @@
 <?php
 
-// Set the Laravel application path
 $appPath = __DIR__ . '/../';
 
-// Bootstrap Laravel
 require_once $appPath . 'vendor/autoload.php';
 
 $app = require_once $appPath . 'bootstrap/app.php';
 
-// Set the request context
 $request = Illuminate\Http\Request::capture();
 $app->instance('request', $request);
 
-// Handle the request
 $response = $app->handle($request);
 $response->send();
 
