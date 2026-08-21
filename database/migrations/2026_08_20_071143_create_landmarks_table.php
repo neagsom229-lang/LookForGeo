dd3bcebb268c7c1c90928902d40780b6de7f074c
Chheangsamnang <chheangsamnang.wu@gmail.com>
2026-08-21 21:56:12 +0700

    Fix migrations: proper order - users, analyses, landmarks

--- database/migrations/2026_08_20_071143_create_landmarks_table.php
+++ database/migrations/2026_08_20_071143_create_landmarks_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('landmarks', function (Blueprint $table) {
             $table->id();
@@ -30,7 +30,7 @@ return new class extends Migration
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('landmarks');
     }
