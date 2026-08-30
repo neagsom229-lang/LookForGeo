100268b6b0fdc087b52fa7fc3b96021695ffed38
Chheangsamnang <chheangsamnang.wu@gmail.com>
2026-08-30 12:22:34 +0700

    Exclude all API routes from CSRF verification

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'api/*',
-        'api/login',
-        'api/register',
-    ];
+    'api/*',
+    'api/login',
+    'api/register',
+];
 }
