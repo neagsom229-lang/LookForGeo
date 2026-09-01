e260e84056004663994dac2a4e50a8b032310920
Chheangsamnang <chheangsamnang.wu@gmail.com>
2026-09-01 21:11:09 +0700

    Clean up CSP header: remove duplicate policy

--- app/Http/Middleware/ForceCsp.php
+++ app/Http/Middleware/ForceCsp.php
@@ -6,19 +6,16 @@ use Closure;
 
 class ForceCsp
 {
-public function handle($request, Closure $next)
-{
-    // Temporary debug - remove after testing
-    \Log::info('ForceCsp middleware is running!');
-    
-    $response = $next($request);
-    $response->headers->set('Content-Security-Policy', 
-        "script-src 'self' https://cdnjs.cloudflare.com https://unpkg.com https://cdn.jsdelivr.net 'unsafe-inline'; " .
-        "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; " .
-        "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; " .
-        "connect-src 'self' https://lookforgeo.onrender.com; " .
-        "img-src * data:;"
-    );
-    return $response;
-}
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('Content-Security-Policy', 
+            "script-src 'self' https://cdnjs.cloudflare.com https://unpkg.com https://cdn.jsdelivr.net 'unsafe-inline'; " .
+            "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://unpkg.com; " .
+            "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; " .
+            "connect-src 'self' https://lookforgeo.onrender.com https://unpkg.com; " .
+            "img-src * data:;"
+        );
+        return $response;
+    }
 }
